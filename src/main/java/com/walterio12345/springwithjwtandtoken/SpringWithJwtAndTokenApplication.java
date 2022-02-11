f7e8e32bd89e29382b1effee1b1fae4f87cb9301
Victor Schweizer <walterio12345@gmail.com>
2022-02-11 10:12:20 -0300

    final

--- src/main/java/com/walterio12345/springwithjwtandtoken/SpringWithJwtAndTokenApplication.java
+++ src/main/java/com/walterio12345/springwithjwtandtoken/SpringWithJwtAndTokenApplication.java
@@ -1,20 +1,14 @@
 package com.walterio12345.springwithjwtandtoken;
 
-import com.walterio12345.springwithjwtandtoken.domain.Role;
-import com.walterio12345.springwithjwtandtoken.domain.User;
-import com.walterio12345.springwithjwtandtoken.service.UserService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 
-import java.util.ArrayList;
+
 
 @SpringBootApplication
 public class SpringWithJwtAndTokenApplication  {
